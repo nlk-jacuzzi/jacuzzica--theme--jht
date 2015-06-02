0f4ca2a93c5025be777a6912a7fa59399933cc42
Tim Spinks <tim@ninthlink.com>
2015-06-02 15:52:47 -0700

    promo fix

--- front-page.php
+++ front-page.php
@@ -19,7 +19,7 @@ get_header(); ?>
         </div>
         */ ?>
         <!-- END PROMO -->
-        <div class="slide0 slide" style="display: none; position: absolute; top: 0; left: 0; z-index: 2; background-image: url(<?php bloginfo('template_url'); ?>/images/heros/Final-j-500-CanEng.jpg);">
+        <div class="slide0 slide" style="position: absolute; top: 0; left: 0; z-index: 2; background-image: url(<?php bloginfo('template_url'); ?>/images/heros/Final-j-500-CanEng.jpg);">
             <div class="wrap">
                 <a href="<?php echo get_bloginfo('url'); ?>/j-500/" target="_blank"  style="position: absolute; color: transparent; width: 190px; height: 40px; top: 307px; left: 21px;">
                 </a>
